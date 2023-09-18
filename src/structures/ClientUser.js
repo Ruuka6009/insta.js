f9189b88ad45e4683e5f0fe5b30cfc824e5301cd
Ruuka <128378374+Ruuka6009@users.noreply.github.com>
2023-09-19 01:50:28 +0200

    Update ClientUser.js
    Added the possibility of posting pictures + tag a person anywhere in the picture.

--- src/structures/ClientUser.js
+++ src/structures/ClientUser.js
@@ -1,62 +1,72 @@
-const User = require('./User')
+const User = require('./User');
 
 /**
- * Represents the logged in client's Instagram user.
+ * Represents the logged-in client's Instagram user.
  * @extends {User}
  */
 class ClientUser extends User {
-    /**
-     * @param {Client} client The instantiating client
-     * @param {object} data The data for the client user.
-     */
-    constructor (client, data) {
-        super(client, data)
-        this._patch(data)
+  /**
+   * @param {Client} client The instantiating client
+   * @param {object} data The data for the client user
+   */
+  constructor(client, data) {
+    super(client, data);
+    this.allowContactsSync = data.allowContactsSync;
+    this.phoneNumber = data.phoneNumber;
+  }
+
+  /**
+   * Change the bot's biography
+   * @param {string} content The new biography
+   * @returns {Promise<string>} The new biography
+   */
+  async setBiography(content) {
+    await this.client.ig.account.setBiography(content);
+    return (this.biography = content);
+  }
+
+  /**
+   * Post a picture, with optional tagging
+   * @param {Buffer} buffer The image buffer
+   * @param {string} captiontxt The caption text
+   * @param {string} [nametxt] The name to tag (optional)
+   * @param {number} [x] The x-coordinate for the tag (optional)
+   * @param {number} [y] The y-coordinate for the tag (optional)
+   * @returns {Promise<boolean>} True if successful, false otherwise
+   */
+  async postPicture(buffer, captiontxt, nametxt, x, y) {
+    const postConfig = {
+      file: buffer,
+      caption: captiontxt,
+    };
+
+    if (nametxt && x !== undefined && y !== undefined) {
+      const tag = await this.createTag(nametxt, x, y);
+      postConfig.usertags = { in: [tag] };
     }
 
-    _patch (data) {
-        super._patch(data)
-        /**
-         * @type {boolean}
-         * Whether the user has enabled contact synchronization
-         */
-        this.allowContactsSync = data.allowContactsSync
-        /**
-         * @type {string}
-         * The phone number of the user
-         */
-        this.phoneNumber = data.phoneNumber
-    }
+    const { status } = await this.client.ig.publish.photo(postConfig);
+    return status === 'ok';
+  }
 
-    get follow () { return undefined }
-    get unfollow () { return undefined }
-    get block () { return undefined }
-    get unblock () { return undefined }
-    get approveFollow () { return undefined }
-    get denyFollow () { return undefined }
-    get removeFollower () { return undefined }
-    get send () { return undefined }
+  /**
+   * Create a tag for a user
+   * @param {string} name The name of the user to tag
+   * @param {number} x The x-coordinate for the tag
+   * @param {number} y The y-coordinate for the tag
+   * @returns {Promise<object>} The tag object
+   */
+  async createTag(name, x, y) {
+    const { pk } = await this.client.ig.user.searchExact(name);
+    return {
+      user_id: pk,
+      position: [this.clamp(x), this.clamp(y)],
+    };
+  }
 
-    /**
-     * Change the bot's biography
-     * @param {string} content The new biography
-     * @returns {Promise<string>} The new biography
-     */
-    async setBiography (content) {
-        this.biography = content
-        await this.client.ig.account.setBiography(content)
-        return this.biography
-    }
-
-    toJSON () {
-        return {
-            ...super.toJSON(),
-            ...{
-                allowContactsSync: this.allowContactsSync,
-                phoneNumber: this.phoneNumber
-            }
-        }
-    }
+  clamp(value, min = 0.0001, max = 0.9999) {
+    return Math.max(Math.min(value, max), min);
+  }
 }
 
-module.exports = ClientUser
+module.exports = ClientUser;
